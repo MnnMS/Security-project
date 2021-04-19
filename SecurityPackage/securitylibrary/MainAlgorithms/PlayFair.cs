d4967812924497e2d4aa8789b4d7397bcdaaa852
MohamedAdel769 <mohamed.adel90211@gmail.com>
2021-04-19 15:28:04 +0200

    created matrix(key)
    And removed duplicate characters from plainText.

--- SecurityPackage/securitylibrary/MainAlgorithms/PlayFair.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/PlayFair.cs
@@ -15,7 +15,62 @@ namespace SecurityLibrary
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            string processedText = "";
+            for(int i = 0; i < plainText.Length; i += 2)
+            {
+                processedText += plainText[i];
+                if (plainText[i] == plainText[i + 1])
+                    processedText += 'x';
+                processedText += plainText[i + 1];
+            }
+            if (processedText.Length % 2 != 0) processedText += 'x';
+            char[,] matrix = getKey(key);
+            // same row 
+
+            // same column
+
+            // diagonal
+        }
+
+        private char[,] getKey(string keyword)
+        {
+            char[,] matrix = new char[5,5];
+            Dictionary<char, int> alpha = new Dictionary<char, int>();
+            int ind = 0;
+            string uniqueText = "";
+            char s = 'a';
+            for(int i = 0; i < 26; i++)
+            {
+                alpha.Add(s, 0);
+                s++;
+            }
+            for(int i = 0; i < keyword.Length; i++)
+            {
+                char curr = keyword[i];
+                if (alpha[curr] == 0)
+                {
+                    uniqueText += curr;
+                    alpha[curr] = 1;
+                }
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    if (ind < uniqueText.Length)
+                    {
+                        matrix[i, j] = uniqueText[ind];
+                        ind++;
+                    }
+                    else
+                    {
+                        char c = alpha.First(x => x.Value == 0).Key;
+                        matrix[i, j] = c;
+                        alpha[c] = 1;
+                    }
+                }
+            }
+            return matrix;
         }
     }
 }
