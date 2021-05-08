b06db97c61926d33e05e8570375cdd4e034463ae
mennatalah <49452743+mennatalah@users.noreply.github.com>
2021-05-08 04:50:46 +0200

    Columnar Decrypt

--- SecurityPackage/securitylibrary/MainAlgorithms/Columnar.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/Columnar.cs
@@ -15,7 +15,33 @@ namespace SecurityLibrary
 
         public string Decrypt(string cipherText, List<int> key)
         {
-            throw new NotImplementedException();
+            int rowLength = key.Count;
+            int ctLength = cipherText.Length;
+            int columnLength = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(ctLength) / rowLength));
+            int cellNotHaveValue = (rowLength * columnLength) - ctLength;
+            char[] plainText = new char[ctLength];
+            int planIndex = 0;
+            int index;
+            for(int i =0;i< rowLength; i++)
+            {     
+                int j = 0;
+                index = (key[i]-1) * columnLength;
+                if (rowLength - i <= cellNotHaveValue)
+                {
+                    cipherText=cipherText.Insert(index+columnLength-1, "*");
+                }
+                planIndex = i;
+                while (j < columnLength) {
+                    if ( index>= cipherText.Length|| planIndex>= ctLength|| cipherText[index] == '*')
+                        break;
+                    plainText[planIndex] +=cipherText[index];
+                    index++;
+                    planIndex += rowLength;
+                    j++;
+                }
+
+            }
+            return new string(plainText);
         }
 
         public string Encrypt(string plainText, List<int> key)
