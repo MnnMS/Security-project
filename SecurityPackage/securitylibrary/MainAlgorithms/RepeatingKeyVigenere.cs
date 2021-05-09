4d1c20b7f7d531f379753526d38936752b4ef43a
mennatalah <49452743+mennatalah@users.noreply.github.com>
2021-05-09 22:00:35 +0200

    Repeating key  Vigenere Enc

--- SecurityPackage/securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -8,6 +8,45 @@ namespace SecurityLibrary
 {
     public class RepeatingkeyVigenere : ICryptographicTechnique<string, string>
     {
+        public string GetKey(string key, string plainText)
+        {
+            int lenOfKey = key.Length;
+            int lenOfPt = plainText.Length;
+            int n = lenOfPt - lenOfKey;
+            int ind = 0;
+
+            while (n > 0)
+            {
+                key = key.Insert(key.Length, key[ind].ToString());
+                n--;
+                ind = (ind == lenOfKey - 1) ? 0 : ind += 1;
+
+            }
+
+            return key;
+
+        }
+
+        public char[,] GetVigenereTable()
+        {
+            char start = 'A';
+            char value = start;
+            char[,] vigenereTable = new char[26, 26];
+
+            for (int i = 0; i < 26; i++)
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    vigenereTable[i, j] = value;
+                    if (value < 'Z') value++;
+                    else value = 'A';
+
+                }
+                start++;
+                value = start;
+            }
+            return vigenereTable;
+        }
         public string Analyse(string plainText, string cipherText)
         {
             throw new NotImplementedException();
@@ -20,7 +59,19 @@ namespace SecurityLibrary
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            string key_stream = GetKey(key, plainText);
+            char[,] vigenereTable = GetVigenereTable();
+            string ct = string.Empty;
+            int length = plainText.Length;
+            for (int i = 0; i < length; i++)
+            {
+                int ptInd = ((int)plainText[i]) % 97;
+                int keyInd = ((int)key_stream[i]) % 97;
+                ct += vigenereTable[ptInd, keyInd];
+            }
+
+
+            return ct;
         }
     }
 }
