8711909351f4c125180caa4e43d686b83e75ce5b
MohamedAdel769 <mohamed.adel90211@gmail.com>
2021-05-10 17:18:35 +0200

    HillCipher 🔥

--- SecurityPackage/securitylibrary/MainAlgorithms/HillCipher.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/HillCipher.cs
@@ -11,93 +11,81 @@ namespace SecurityLibrary
     /// </summary>
     public class HillCipher :  ICryptographicTechnique<List<int>, List<int>>
     {
-        private int[,] toMatrix(List<int> arr, int n, int m)
+        public List<int> Analyse(List<int> plainText, List<int> cipherText)
         {
-            int[,] mat = new int[n, m];
-            int ind = 0;
-
-            for (int i = 0; i < n; i++)
+            List<int> key = new List<int>();
+            List<List<int>> choices = generateKey();
+            bool flag = false;
+            for(int i = 0; i < choices.Count; i++)
             {
-                for(int j = 0; j < m; j++)
+                Matrix choice = new Matrix(choices[i], 2, 2);
+                int det = choice.determ2();
+                if ((det==1 || det==-1)&&(isValid(choices[i], plainText, cipherText)))
                 {
-                    mat[i, j] = arr[ind++];
+                    flag = true;
+                    key = choices[i];
+                    break;
                 }
             }
-
-            return mat;
+            if (!flag)
+                throw new InvalidAnlysisException();
+  
+            return key;
         }
 
-        private List<int> toList(int[,] mat)
+        public List<int> Decrypt(List<int> cipherText, List<int> key)
         {
-            int n = mat.GetLength(0);
-            int m = mat.GetLength(1);
-            List<int> arr = new List<int>(n*m);
-
-            for (int i = 0; i < n; i++)
+            List<int> plain = new List<int>();
+            int m = (int)Math.Sqrt(key.Count);
+            Matrix cipher = new Matrix(cipherText, m, cipherText.Count / m);
+            Matrix keyMat = new Matrix(key, m, m);
+            if(m == 2) // 2x2 inverse
             {
-                for (int j = 0; j < m; j++)
-                {
-                    arr.Add(mat[i, j]);
-                }
+                Matrix keyInv = keyMat.inverse();
+                Matrix plainMat = keyInv.multiply(cipher);
+                plain = plainMat.toList();         
             }
-
-            return arr;
-        }
-
-        private int mod(int x)
-        {
-            while (x < 0) x += 26;
-            return x % 26;
-        }
-
-        private int[,] inverse2(int [,] matrix)
-        {
-            int m = matrix.GetLength(0);
-            int[,] inverse = new int[m,m];
-            int a = matrix[0, 0], b = matrix[0,1], c = matrix[1,0], d = matrix[1,1];
-            int det = determ2(matrix);
-            if (det != -1 && det != 1)
-                throw new Exception();
-
-
-            int x = 1 / det;
-            inverse[0, 0] = mod(d * x);
-            inverse[0, 1] = mod(-1 * b * x);
-            inverse[1, 0] = mod(-1 * c * x);
-            inverse[1, 1] = mod(a * x);
-
-            return inverse;
-        }
-
-        private int[,] inverse3(int[,] matrix, int b)
-        {
-            int m = matrix.GetLength(0);
-            int[,] inverse = new int[m, m];
-            for (int i = 0; i < m; i++)
+            else // 3x3 inverse 
             {
-                for (int j = 0; j < m; j++)
+                int det = keyMat.determ3();
+                if (gcd(det, 1) == 1)
                 {
-                    int subDet = (m == 3 ? determ2(getSubMat(matrix, i, j)) : 1);
-                    inverse[i, j] = mod(b * (int)Math.Pow(-1, i + j) * subDet);
+                    Matrix keyInv = keyMat.inverse();
+                    Matrix plainMat = keyInv.multiply(cipher);
+                    plain = plainMat.toList();
                 }
+                else
+                    throw new Exception();
             }
-
-            return transpose(inverse);
+            return plain;
         }
 
-        private int[,] transpose(int [,] mat)
+
+        public List<int> Encrypt(List<int> plainText, List<int> key)
         {
-            int n = mat.GetLength(0);
-            int m = mat.GetLength(1);
-            int[,] trans = new int[m, n];
-            for (int i = 0; i < n; i++)
+            int m =(int)Math.Sqrt(key.Count());
+            Matrix keyMat = new Matrix(key, m, m);
+            int n = (int)Math.Ceiling((double)(plainText.Count() / m));
+            Matrix ptMat = new Matrix(plainText,m,n);
+            Matrix resultMat = keyMat.multiply(ptMat);
+            List<int> ct = resultMat.toList();
+            return ct;
+        }
+
+        public List<int> Analyse3By3Key(List<int> plainText, List<int> cipherText)
+        {
+            List<int> key = new List<int>();
+            Matrix cipher = new Matrix(cipherText, 3, cipherText.Count / 3);
+            Matrix plain = new Matrix(plainText, 3, plainText.Count / 3);
+
+            int det = plain.determ3();
+            if (gcd(det, 1) == 1)
             {
-                for (int j = 0; j < m; j++)
-                {
-                    trans[j, i] = mat[i, j];
-                }
+                Matrix plainInv = plain.inverse();
+                Matrix keyMat = plainInv.multiply(cipher.T());
+                key = keyMat.toList();
             }
-            return trans;
+            return key;
         }
 
         private List<List<int>> generateKey()
@@ -105,89 +93,20 @@ namespace SecurityLibrary
             List<List<int>> result = new List<List<int>>();
             for (int i = 0; i < 26; i++)
             {
-                for(int j = 0; j < 26; j++)
-                {    
-                    for(int k = 0; k < 26; k++)
+                for (int j = 0; j < 26; j++)
+                {
+                    for (int k = 0; k < 26; k++)
                     {
                         for (int l = 0; l < 26; l++)
                         {
-                            List<int> key = new List<int>(new int[]{i, j, k, l});
+                            List<int> key = new List<int>(new int[] { i, j, k, l });
                             result.Add(key);
                         }
-                    }          
-                   
-                }
-            }
-            return result;
-        }
-
-        private int[,] multiplyMat(int[,] key, int[,] textMat)
-        {
-            int n = textMat.GetLength(1);
-            int m = textMat.GetLength(0);
-            int keyDim = key.GetLength(0);
-            List<int> cipher = toList(textMat);
-            int[,] result ;
-            List<int> res = new List<int>();
-            int nOfCols = cipher.Count / keyDim;
-            for(int i = 0; i < nOfCols; i++)
-            {
-                List<int> col = cipher.GetRange(i* keyDim, keyDim) ;
-                int[] resCol = new int[keyDim];
-                for(int k = 0; k < keyDim; k++)
-                {
-                    int cell = 0;
-                    for (int j = 0; j < keyDim; j++)
-                    {
-                        cell += col[j] * key[k, j];
                     }
-                    resCol[k] = mod(cell);
-                }
-                res.AddRange(resCol);
-            }
-            result = toMatrix(res, m, n);
-            return result;
-        }
 
-        private int[,] getSubMat(int [,] mat, int i, int j)
-        {
-            int m = mat.GetLength(0);
-            List<int> sub = new List<int>();
-            int[,] subMat = new int[m - 1, m - 1];
-            for (int k = 0; k < m; k++)
-            {
-                for (int n = 0; n < m; n++)
-                {
-                    if (k != i && n != j)
-                        sub.Add(mat[k, n]);
                 }
             }
-            return toMatrix(sub, 2, 2);
-        }
-
-        private int determ3(int [,] mat)
-        {
-            int m = mat.GetLength(0);
-            int result = 0;
-            int sign = 1;
-
-            for(int i = 0; i < m; i++)
-            {
-                int x = mat[0, i] * sign;
-                List<int> sub = new List<int>();
-                int[,] subMat = getSubMat(mat, 0, i);
-                int subDet = determ2(subMat);
-                result += x * subDet;
-                sign *= -1;
-            }
             return result;
-        }
-
-        private int determ2(int [,] mat)
-        {
-            int d1 = mat[0,0] * mat[1,1];
-            int d2 = mat[0, 1] * mat[1, 0];
-            return d1 - d2;
         }
 
         private static int gcd(int n1, int n2)
@@ -209,97 +128,176 @@ namespace SecurityLibrary
             return true;
         }
 
-        public List<int> Analyse(List<int> plainText, List<int> cipherText)
+    }
+
+    public class Matrix
+    {
+        int n, m;
+        public int[,] value;
+
+        public Matrix(int n, int m)
         {
-            List<int> key = new List<int>();
-            List<List<int>> choices = generateKey();
-            bool flag = false;
-            for(int i = 0; i < choices.Count; i++)
+            this.n = n;
+            this.m = m;
+            value = new int[n, m];
+        }
+
+        public Matrix(List<int> arr, int n, int m)
+        {
+            this.n = n;
+            this.m = m;
+            value = new int[n, m];
+            int ind = 0;
+
+            for (int i = 0; i < n; i++)
             {
-                int det = determ2(toMatrix(choices[i], 2, 2));
-                if ((det==1 || det==-1)&&(isValid(choices[i], plainText, cipherText)))
+                for (int j = 0; j < m; j++)
                 {
-                    flag = true;
-                    key = choices[i];
-                    break;
+                    value[i, j] = arr[ind++];
                 }
             }
-            if (!flag)
-                throw new InvalidAnlysisException();
-  
-            return key;
         }
 
-        public List<int> Decrypt(List<int> cipherText, List<int> key)
+        public List<int> toList()
         {
-            List<int> plain = new List<int>();
-            int m = (int)Math.Sqrt(key.Count);
-            int[,] cipher = (toMatrix(cipherText, m, cipherText.Count/m)), keyMat = toMatrix(key, m, m);
-            if(m == 2) // 2x2 inverse
+            int n = value.GetLength(0);
+            int m = value.GetLength(1);
+            List<int> arr = new List<int>(n * m);
+
+            for (int i = 0; i < n; i++)
             {
-                int [,] keyInv = inverse2(keyMat);
-                int[,] plainMat = multiplyMat(keyInv, cipher);
-                plain = toList(plainMat);         
-            }
-            else // 3x3 inverse 
-            {
-                int det = determ3(keyMat);
-                if (gcd(det, 1) == 1)
+                for (int j = 0; j < m; j++)
                 {
-                    int b = 1;
-                    while (mod(b * det) != 1)
-                        b++;
-                    int[,] keyInv = inverse3(keyMat, b);
-                    int[,] plainMat = multiplyMat(keyInv, cipher);
-                    plain = toList(plainMat);
+                    arr.Add(value[i, j]);
                 }
-                else
-                    throw new Exception();
             }
-            return plain;
+
+            return arr;
         }
 
-
-        public List<int> Encrypt(List<int> plainText, List<int> key)
+        private int mod(int x)
         {
-            int m =(int)Math.Sqrt(key.Count());
-            int[,] keyMat = toMatrix(key, m, m);
-            int n = (int)Math.Ceiling((double)(plainText.Count() / m));
-            int[,] ptMat = toMatrix(plainText,m,n);
-            int[,] resultMat = multiplyMat(keyMat, ptMat);
-            int nOfRows = resultMat.GetLength(0);
-            int nOfCols = resultMat.GetLength(1);
-            List<int> ct = new List<int>();
-
-            for (int i=0;i< nOfRows; i++)
-                for(int j=0;j< nOfCols; j++) {
-
-                    ct.Add((int)resultMat[i,j] % 32);
-                }
-
-
-            return ct;
-
+            while (x < 0) x += 26;
+            return x % 26;
         }
 
-        public List<int> Analyse3By3Key(List<int> plainText, List<int> cipherText)
+        public Matrix multiply(Matrix textMat)
         {
-            List<int> key = new List<int>();
-            int[,] cipher = (toMatrix(cipherText, 3, cipherText.Count / 3)), plain = (toMatrix(plainText, 3, plainText.Count / 3));
-            //int[,] plainTrns = transpose(plain);
-            int det = determ3(plain);
-            int[,] plainInv = new int[3,3];
-            if (gcd(det, 1) == 1)
+            int n = textMat.m;
+            int m = textMat.n;
+            int keyDim = this.n;
+            List<int> cipher = textMat.toList();
+            List<int> res = new List<int>();
+            int nOfCols = cipher.Count / keyDim;
+            for (int i = 0; i < nOfCols; i++)
             {
+                List<int> col = cipher.GetRange(i * keyDim, keyDim);
+                int[] resCol = new int[keyDim];
+                for (int k = 0; k < keyDim; k++)
+                {
+                    int cell = 0;
+                    for (int j = 0; j < keyDim; j++)
+                    {
+                        cell += col[j] * this.value[k, j];
+                    }
+                    resCol[k] = mod(cell);
+                }
+                res.AddRange(resCol);
+            }
+            return new Matrix(res, m, n);
+        }
+
+        public Matrix getSubMat(int i, int j)
+        {
+            int m = this.n;
+            List<int> sub = new List<int>();
+            for (int k = 0; k < m; k++)
+            {
+                for (int n = 0; n < m; n++)
+                {
+                    if (k != i && n != j)
+                        sub.Add(value[k, n]);
+                }
+            }
+            return new Matrix(sub, 2, 2);
+        }
+
+        public int determ3()
+        {
+            int m = n;
+            int result = 0;
+            int sign = 1;
+
+            for (int i = 0; i < m; i++)
+            {
+                int x = value[0, i] * sign;
+                Matrix subMat = getSubMat(0, i);
+                int subDet = subMat.determ2();
+                result += x * subDet;
+                sign *= -1;
+            }
+            return result;
+        }
+
+        public int determ2()
+        {
+            int d1 = value[0, 0] * value[1, 1];
+            int d2 = value[0, 1] * value[1, 0];
+            return d1 - d2;
+        }
+
+        public Matrix inverse()
+        {
+            int m = n;
+            Matrix inverse = new Matrix(m, m);
+            if (m == 3)
+            {
+                int det = determ3();
                 int b = 1;
                 while (mod(b * det) != 1)
                     b++;
-                plainInv = inverse3(plain, b);
-                int[,] keyMat = multiplyMat( plainInv,transpose(cipher));
-                key = toList(keyMat);
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        Matrix subMatrix = getSubMat(i, j);
+                        int subDet = (m == 3 ? subMatrix.determ2() : 1);
+                        inverse.value[i, j] = mod(b * (int)Math.Pow(-1, i + j) * subDet);
+                    }
+                }
+                inverse = inverse.T();
             }
-            return key;
+            else
+            {
+                int a = value[0, 0], b = value[0, 1], c = value[1, 0], d = value[1, 1];
+                int det = determ2();
+                if (det != -1 && det != 1)
+                    throw new Exception();
+
+
+                int x = 1 / det;
+                inverse.value[0, 0] = mod(d * x);
+                inverse.value[0, 1] = mod(-1 * b * x);
+                inverse.value[1, 0] = mod(-1 * c * x);
+                inverse.value[1, 1] = mod(a * x);
+            }
+                   
+            return inverse;
         }
 
+        public Matrix T()
+        {
+            int n = this.n;
+            int m = this.m;
+            Matrix trans = new Matrix(m, n);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    trans.value[j, i] = value[i, j];
+                }
+            }
+            return trans;
+        }
     }
 }
