b2c4ce0946887a591e80be66c1508a35a8beda0f
MohamedAdel769 <mohamed.adel90211@gmail.com>
2021-05-27 00:13:35 +0200

    KeySchedule DONE 💣

--- SecurityPackage/securitylibrary/AES/AES.cs
+++ SecurityPackage/securitylibrary/AES/AES.cs
@@ -12,13 +12,14 @@ namespace SecurityLibrary.AES
     /// </summary>
     public class AES : CryptographicTechnique
     {
-        int block_size = 16;
+        static int block_size = 16;
         int num_of_rounds = 10;
-        int row_col = 4;
+        int row_col = (int)Math.Sqrt(block_size);
         char[] seps = { '\t', ' ' };
         Dictionary<string, int> Hex = new Dictionary<string, int>(){
             { "a", 10 }, { "b", 11 }, { "c", 12 }, { "d", 13 }, { "e", 14 }, { "f", 15 }
         };
+        List<string> rcon;
 
         public override string Decrypt(string cipherText, string key)
         {
@@ -27,6 +28,7 @@ namespace SecurityLibrary.AES
 
         public override string Encrypt(string plainText, string key)
         {
+            rcon = new List<string>();
             // --- (plain to blocks) ---
 
             // --- (intial) ---
@@ -47,6 +49,68 @@ namespace SecurityLibrary.AES
             throw new NotImplementedException();
         }
 
+        public void UpdateRcon(int index)
+        {
+            if (index == 0)
+            {
+                rcon.Add("01");
+                return;
+            }
+            int[] res = Byte_to_Int(rcon[index-1]);
+            int R_prev = res[0] * 10 + res[1];
+            int new_R = R_prev * 2;
+            if (R_prev >= 128)
+            {
+                new_R = new_R ^ 283;
+            }
+            rcon.Add(new_R.ToString("x"));
+        }
+
+        public string[,] KeySchedule(string[,] prev_key, int round)
+        {
+            string[,] new_key = new string[row_col, row_col];
+            string[,] prev_W = new string[row_col, 1];
+            for(int i = 0; i < row_col; i++)
+            {
+                prev_W[i, 1] = (prev_key[i, row_col - 1]);
+            }
+
+            string temp = prev_W[0, 1];
+            prev_W[0, 1] = prev_W[row_col - 1, 1];
+            prev_W[row_col - 1, 1] = temp;
+
+            prev_W = SubBytes(prev_W);
+            UpdateRcon(round - 1);
+            string rcon_val = rcon[round - 1];
+
+            new_key[0, 0] = XOR(XOR(prev_key[0, 0], prev_W[0, 0]), rcon_val);
+
+            for (int i = 1; i < row_col; i++)
+            {
+                new_key[i, 0] = XOR(prev_key[i, 0], prev_W[i, 0]);
+            }
+
+            for(int j = 1; j < row_col; j++)
+            {
+                for(int i = 0; i < row_col; i++)
+                {
+                    new_key[i, j] = XOR(prev_key[i, j], new_key[i, j-1]);
+                }       
+            }
+
+            return new_key;
+        }
+
+        public string XOR(string a, string b)
+        {
+            int[] a_val = Byte_to_Int(a);
+            int[] b_val = Byte_to_Int(b);
+            int A = a_val[0] * 10 + a_val[1];
+            int B = b_val[0] * 10 + b_val[1];
+            int res = A ^ B;
+            return res.ToString("x");
+        }
+
         public string[,] SubBytes(string [,] plain)
         {
             string[,] new_block = new string[row_col, row_col];
@@ -65,6 +129,7 @@ namespace SecurityLibrary.AES
 
             return new_block;
         }
+        
 
         public string[,] AddRoundKey(string[,] plain, string[,] key)
         {
