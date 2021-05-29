a19e135d09978927f689624f44f2ac46d4810262
MohamedAdel769 <mohamed.adel90211@gmail.com>
2021-05-29 23:38:05 +0200

    Update ExtendedEuclid.cs

--- SecurityPackage/securitylibrary/AES/ExtendedEuclid.cs
+++ SecurityPackage/securitylibrary/AES/ExtendedEuclid.cs
@@ -16,7 +16,34 @@ namespace SecurityLibrary.AES
         /// <returns>Mul inverse, -1 if no inv</returns>
         public int GetMultiplicativeInverse(int number, int baseN)
         {
-            throw new NotImplementedException();
+            int A1 = 1, A2 = 0, A3 = baseN;
+            int B1 = 0, B2 = 1, B3 = number;
+
+            while (true)
+            {
+                int q = A3 / B3;
+                int T1 = A1 - (q * B1), T2 = A2 - (q * B2), T3 = A3 - (q * B3);
+
+                A1 = B1;
+                A2 = B2;
+                A3 = B3;
+
+                B1 = T1;
+                B2 = T2;
+                B3 = T3;
+
+                if (B3 == 0)
+                    return -1;
+
+                else if (B3 == 1)
+                {
+                    while(B2 < 0)
+                    {
+                        B2 += baseN;
+                    }
+                    return B2;
+                }
+            }
         }
     }
 }
